b68d9d868eebbdb70b9f8422ed5fe73a9773e6ae
Ubuntu <ubuntu@ip-172-31-12-249.ap-southeast-2.compute.internal>
2013-08-11 15:55:59 +0000

    result part b update

--- search_result.php
+++ search_result.php
@@ -5,17 +5,17 @@
 </head>
 
 <?php
-    $criteria = $_GET["criteria"];//all = no criteria; some = some criterias
-    $winename = $_GET["winename"];
-    $wineryname = $_GET["wineryname"];
-    $region = $_GET["region"];
-    $grapeVariety = $_GET["grapeVariety"];
-    $yearFrom = $_GET["yearFrom"];
-    $yearTo = $_GET["yearTo"];
-    $min_num_instock = $_GET['min_num_instock'];
-    $min_num_ordered = $_GET['min_num_ordered'];
-    $min_cost = $_GET["min_cost"];
-    $max_cost = $_GET["max_cost"];
+    $criteria = $get["criteria"];//all = no criteria; some = some criterias
+    $nameOfWine = $get["nameOfWine"];
+    $nameOfWinery = $get["nameOfWinery"];
+    $region = $get["region"];
+    $grapeVariety = $get["grapeVariety"];
+    $yearFrom = $get["yearFrom"];
+    $yearTo = $get["yearTo"];
+    $num_of_min_in_stock = $get['num_of_min_in_stock'];
+    $num_of_min_order = $get['num_of_min_order'];
+    $min_cost = $get["min_cost"];
+    $max_cost = $get["max_cost"];
     
     $query = 'SELECT DISTINCT wine.wine_id, wine_name, year, winery_name, region_name, cost, on_hand, SUM(qty) qty, SUM(price)
               FROM wine, winery, region, inventory, items, wine_variety
@@ -36,15 +36,15 @@
         /*
             Piece together the SQL statement
         */
-        if($winename != '') 
+        if($nameOfWine != '') 
 		{
-            $winename = str_replace("'", "''", $winename);
-            $query .= " AND wine.wine_name LIKE '%$winename%'";
+            $nameOfWine = str_replace("'", "''", $nameOfWine);
+            $query .= " AND wine.wine_name LIKE '%$nameOfWine%'";
         }
-        if($wineryname != '') 
+        if($nameOfWinery != '') 
 		{
-            $wineryname = str_replace("'", "''", $wineryname);
-            $query .= " AND winery_name LIKE '%$wineryname%'";
+            $nameOfWinery = str_replace("'", "''", $nameOfWinery);
+            $query .= " AND winery_name LIKE '%$nameOfWinery%'";
         }
         if($region != 1) 
 		{
@@ -64,9 +64,9 @@
 		{
             $query .= " AND year <= $yearTo";
         }
-        if($min_num_instock != 0) 
+        if($num_of_min_in_stock != 0) 
 		{
-            $query .= " AND on_hand >= $min_num_instock";
+            $query .= " AND on_hand >= $num_of_min_in_stock";
         }
         if($min_cost != 0) 
 		{
@@ -76,10 +76,10 @@
 		{
             $query .= " AND cost <= $max_cost";
         }
-        if($min_num_ordered != 0) 
+        if($num_of_min_order != 0) 
 		{
             $query .= " GROUP BY items.wine_id
-                        HAVING qty >= $min_num_ordered
+                        HAVING qty >= $num_of_min_order
                         ORDER BY wine_name, year LIMIT 200";
         }
         else $query .= ' GROUP BY items.wine_id
@@ -112,70 +112,69 @@
     }
 ?>
 
-
 <body>
 
 <div>
-        <div>
-        	<div><I><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Result Display</h3></I></div>
-            <div><a href="searching.php" title="Back">Back to Search Page</a></div>
-            <div>
-                <?php
-                    if(!$result) echo "<div class='noResult'>No records match your search criteria.</div>";
-                    else 
+	<div>
+		<div><I><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Result Display</h3></I></div>
+		<div><a href="searching.php" title="Back">Back to Search Page</a></div>
+		<div>
+			<?php
+				if(!$result) echo "<div class='noResult'>No records match your search criteria.</div>";
+				else 
+				{
+			?>
+			<table>
+				<tr>
+					<th>Wine Name</th>
+					<th>Grape Variety</th>
+					<th>Year</th>
+					<th>Winery</th>
+					<th>Region</th>
+					<th>Cost in<br/>Inventory</th>
+					<th>Number of<br/>Bottles Available</th>
+					<th>Total<br/>Stock Sold</th>
+					<th>Total<br/>Sales Revenue</tr>
+				<?php
+					while($row = mysql_fetch_row($result)) 
 					{
-                ?>
-                <table>
-                    <tr>
-                        <th>Wine Name</th>
-                        <th>Grape Variety</th>
-                        <th>Year</th>
-                        <th>Winery</th>
-                        <th>Region</th>
-                        <th>Cost in<br/>Inventory</th>
-                        <th>Number of<br/>Bottles Available</th>
-                        <th>Total<br/>Stock Sold</th>
-                        <th>Total<br/>Sales Revenue</tr>
-                    <?php
-                        while($row = mysql_fetch_row($result)) 
+				?>
+				<tr>
+					<td><?php echo $row[1]; ?></td>
+					<td style="line-height: 16px">
+					<?php
+						$query = "SELECT variety FROM wine_variety, grape_variety
+								  WHERE wine_variety.wine_id = $row[0] AND
+								  wine_variety.variety_id = grape_variety.variety_id
+								  ORDER BY variety";
+						$varieties = mysql_query($query, $dbconn);
+						$str = "";
+						while($variety = mysql_fetch_row($varieties)) 
 						{
-                    ?>
-                    <tr>
-                        <td><?php echo $row[1]; ?></td>
-                        <td style="line-height: 16px">
-                        <?php
-                            $query = "SELECT variety FROM wine_variety, grape_variety
-                                      WHERE wine_variety.wine_id = $row[0] AND
-                                      wine_variety.variety_id = grape_variety.variety_id
-                                      ORDER BY variety";
-                            $varieties = mysql_query($query, $dbconn);
-                            $str = "";
-                            while($variety = mysql_fetch_row($varieties)) 
-							{
-                                $str = "$variety[0], ";
-                            }
-                            //$str = substr($str, 0, strlen($str));
-                            echo substr($str, 0, strlen($str)-2);
-                        ?>
-                        </td>
-                        <td><?php echo $row[2]; ?></td>
-                        <td><?php echo $row[3]; ?></td>
-                        <td><?php echo $row[4]; ?></td>
-                        <td><?php echo '$'. $row[5]; ?></td>
-                        <td><?php echo $row[6]; ?></td>
-                        <td><?php echo $row[7]; ?></td>
-                        <td><?php echo '$'. $row[8]; ?></td>
-                    </tr>
-                    <?php
-                        }
-                    ?>
-                </table>
-                <?php
-                    }
-                ?>
-            </div>
-        </div>
-    </div>
+							$str = "$variety[0], ";
+						}
+						//$str = substr($str, 0, strlen($str));
+						echo substr($str, 0, strlen($str)-2);
+					?>
+					</td>
+					<td><?php echo $row[2]; ?></td>
+					<td><?php echo $row[3]; ?></td>
+					<td><?php echo $row[4]; ?></td>
+					<td><?php echo '$'. $row[5]; ?></td>
+					<td><?php echo $row[6]; ?></td>
+					<td><?php echo $row[7]; ?></td>
+					<td><?php echo '$'. $row[8]; ?></td>
+				</tr>
+				<?php
+					}
+				?>
+			</table>
+			<?php
+				}
+			?>
+		</div>
+	</div>
+</div>
 </body>
 <?php
     mysql_close($dbconn);
